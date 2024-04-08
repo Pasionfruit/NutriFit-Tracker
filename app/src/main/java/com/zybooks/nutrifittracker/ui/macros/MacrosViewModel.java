1858ad2c0113a4374d70481ebd09e5eaf929f792
Abe Pasion <pasionabe@gmail.com>
2024-04-08 15:01:14 -0400

    Macros Fragment - Set up

--- app/src/main/java/com/zybooks/nutrifittracker/ui/macros/MacrosViewModel.java
+++ app/src/main/java/com/zybooks/nutrifittracker/ui/macros/MacrosViewModel.java
@@ -1,16 +1,16 @@
-package com.zybooks.nutrifittracker.ui.home;
+package com.zybooks.nutrifittracker.ui.macros;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class HomeViewModel extends ViewModel {
+public class MacrosViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public HomeViewModel() {
+    public MacrosViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("This is Macros fragment");
     }
 
     public LiveData<String> getText() {
