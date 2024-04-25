ee644a3af059f0778a4ad0b1c2331e3049e2eb39
Abe Pasion <pasionabe@gmail.com>
2024-04-24 21:02:37 -0400

    Adds database for meals, error handling for meals

--- app/src/main/java/com/zybooks/nutrifittracker/MacrosActivity.java
+++ app/src/main/java/com/zybooks/nutrifittracker/MacrosActivity.java
@@ -1,19 +1,32 @@
 package com.zybooks.nutrifittracker;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
-import androidx.preference.PreferenceFragmentCompat;
-import androidx.preference.SwitchPreferenceCompat;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.zybooks.nutrifittracker.R;
+import com.zybooks.nutrifittracker.WorkoutActivity;
+import com.zybooks.nutrifittracker.model.Meal;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MacrosActivity extends AppCompatActivity {
+    private static final String[] MEAL_TYPES = {"Breakfast", "Lunch", "Snack", "Dinner"};
+
+    private List<Meal> mealList = new ArrayList<>(); // List to store meals
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +39,109 @@ public class MacrosActivity extends AppCompatActivity {
             actionBar.setTitle("Macros");
         }
 
+        Spinner mealTypeSpinner = findViewById(R.id.meal_type_spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, MEAL_TYPES);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mealTypeSpinner.setAdapter(adapter);
+
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav_view);
         bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
         bottomNavigationView.setSelectedItemId(R.id.navigation_macros);  // Mark the Macros item as selected
+
+        // Find the add button in your layout
+        Button addButton = findViewById(R.id.add_button);
+
+        // Set a click listener for the add button
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addMealToList(); // Call method to add meal to list
+            }
+        });
+    }
+
+    // Method to add a meal to the list
+    // Method to add a meal to the list
+    // Method to add a meal to the list
+    private void addMealToList() {
+        EditText mealNameEditText = findViewById(R.id.meal_name);
+        Spinner mealTypeSpinner = findViewById(R.id.meal_type_spinner);
+        EditText caloriesEditText = findViewById(R.id.calories);
+        EditText carbsEditText = findViewById(R.id.carbs);
+        EditText fatsEditText = findViewById(R.id.fats);
+        EditText proteinEditText = findViewById(R.id.protein);
+
+        // Retrieve values entered by the user
+        String mealName = mealNameEditText.getText().toString();
+        String mealType = mealTypeSpinner.getSelectedItem().toString();
+        String caloriesStr = caloriesEditText.getText().toString();
+        String carbsStr = carbsEditText.getText().toString();
+        String fatsStr = fatsEditText.getText().toString();
+        String proteinStr = proteinEditText.getText().toString();
+
+        // Flag to indicate if there are any invalid inputs
+        boolean hasInvalidInput = false;
+
+        // Check if any of the input fields are empty
+        if (mealName.isEmpty()) {
+            mealNameEditText.setBackgroundTintList(ColorStateList.valueOf(Color.RED));
+            hasInvalidInput = true;
+        } else {
+            mealNameEditText.setBackgroundTintList(ColorStateList.valueOf(Color.BLUE)); // Revert to default color
+        }
+
+        if (caloriesStr.isEmpty()) {
+            caloriesEditText.setBackgroundTintList(ColorStateList.valueOf(Color.RED));
+            hasInvalidInput = true;
+        } else {
+            caloriesEditText.setBackgroundTintList(ColorStateList.valueOf(Color.BLUE)); // Revert to default color
+        }
+
+        if (carbsStr.isEmpty()) {
+            carbsEditText.setBackgroundTintList(ColorStateList.valueOf(Color.RED));
+            hasInvalidInput = true;
+        } else {
+            carbsEditText.setBackgroundTintList(ColorStateList.valueOf(Color.BLUE)); // Revert to default color
+        }
+
+        if (fatsStr.isEmpty()) {
+            fatsEditText.setBackgroundTintList(ColorStateList.valueOf(Color.RED));
+            hasInvalidInput = true;
+        } else {
+            fatsEditText.setBackgroundTintList(ColorStateList.valueOf(Color.BLUE)); // Revert to default color
+        }
+
+        if (proteinStr.isEmpty()) {
+            proteinEditText.setBackgroundTintList(ColorStateList.valueOf(Color.RED));
+            hasInvalidInput = true;
+        } else {
+            proteinEditText.setBackgroundTintList(ColorStateList.valueOf(Color.BLUE)); // Revert to default color
+        }
+
+        // If any input is invalid, display a toast message and return
+        if (hasInvalidInput) {
+            Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Parse numerical inputs
+        int calories = Integer.parseInt(caloriesStr);
+        int carbs = Integer.parseInt(carbsStr);
+        int fats = Integer.parseInt(fatsStr);
+        int protein = Integer.parseInt(proteinStr);
+
+        // Create a new instance of the Meal class with the retrieved values
+        Meal meal = new Meal(mealName, mealType, calories, carbs, fats, protein);
+
+        // Add the meal to the list
+        mealList.add(meal);
+
+        // Optionally, you can clear the input fields after adding the meal
+        mealNameEditText.getText().clear();
+        caloriesEditText.getText().clear();
+        carbsEditText.getText().clear();
+        fatsEditText.getText().clear();
+        proteinEditText.getText().clear();
     }
 
     private final BottomNavigationView.OnNavigationItemSelectedListener navListener =
--- app/src/main/java/com/zybooks/nutrifittracker/model/Meal.java
+++ app/src/main/java/com/zybooks/nutrifittracker/model/Meal.java
@@ -1,4 +1,69 @@
 package com.zybooks.nutrifittracker.model;
 
 public class Meal {
+    private String name;
+    private String type;
+    private int calories;
+    private int carbs;
+    private int fats;
+    private int protein;
+
+    public Meal(String name, String type, int calories, int carbs, int fats, int protein) {
+        this.name = name;
+        this.type = type;
+        this.calories = calories;
+        this.carbs = carbs;
+        this.fats = fats;
+        this.protein = protein;
+    }
+
+    // Getters and setters for meal attributes
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public int getCarbs() {
+        return carbs;
+    }
+
+    public void setCarbs(int carbs) {
+        this.carbs = carbs;
+    }
+
+    public int getFats() {
+        return fats;
+    }
+
+    public void setFats(int fats) {
+        this.fats = fats;
+    }
+
+    public int getProtein() {
+        return protein;
+    }
+
+    public void setProtein(int protein) {
+        this.protein = protein;
+    }
 }
+
